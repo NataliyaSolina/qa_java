1f7204ff065ee942b29992804e630879c192b6de
Nataliya <nataliya.solina@bsc-ideas.com>
2022-11-21 23:36:36 +0300

    Correct mock test

--- src/test/java/CatMockTest.java
+++ src/test/java/CatMockTest.java
@@ -1,25 +1,25 @@
 import com.example.Cat;
-import org.junit.Assert;
+import com.example.Feline;
+import org.hamcrest.MatcherAssert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnitRunner;
 
+import java.util.List;
+
+import static org.hamcrest.CoreMatchers.containsString;
+
 @RunWith(MockitoJUnitRunner.class)
 public class CatMockTest {
     @Mock
-    Cat cat;
+    Feline feline;
 
     @Test
-    public void getKittensCallMethodRezultOK() {
-        cat.getKittens(Mockito.anyInt());
-        Mockito.verify(cat, Mockito.times(1)).getKittens(Mockito.anyInt());
-    }
-
-    @Test
-    public void getKittensAnyIntRezultAsInStub() {
-        Mockito.when(cat.getKittens(Mockito.anyInt())).thenReturn(6);
-        Assert.assertEquals(6, cat.getKittens(Mockito.anyInt()));
+    public void getKittensAnyIntRezultAsInStub0() throws Exception {
+        Cat cat = new Cat(feline);
+        Mockito.when(feline.eatMeat()).thenReturn(List.of("Животные", "Птицы", "Рыба", "Молоко"));
+        MatcherAssert.assertThat(cat.getFood().toString(), containsString("Молоко"));
     }
 }
